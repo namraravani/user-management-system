ccd73996de2cab5978391c9f86c63de1ed4d3270
Namra Nirajkumar Ravani <92981408+namraravani@users.noreply.github.com>
2023-05-29 00:15:16 -0700

    Update user_update.php

--- task-2/user_update.php
+++ task-2/user_update.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (mysqli_query($conn, $sql)) {
         // Update successful
         $roleid = $newRoleId;
-        $message = "Data Updated Succesfully";
+        $message = "Data Updated Successfully";
         $alertClass = "alert-success";
     } else {
         // Update failed
@@ -44,8 +44,6 @@ mysqli_close($conn);
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        
-
         .navbar-nav {
             margin: 0 auto;
             display: flex;
@@ -61,16 +59,45 @@ mysqli_close($conn);
             margin-top: 100px;
         }
 
-        h1{
-            margin-top : 100px;
+        h1 {
+            margin-top: 100px;
         }
-
-        
     </style>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>user_update</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('form').submit(function(e) {
+                e.preventDefault(); // Prevent form submission
+                var form = $(this);
+                var url = form.attr('action');
+                var formData = form.serialize(); // Serialize the form data
+
+                $.ajax({
+                    type: 'POST',
+                    url: url,
+                    data: formData,
+                    success: function(response) {
+                        console.log(response);
+                        // Handle the response as needed
+                        // For example, display a success message
+                        $('.alert').remove();
+                        form.prepend('<div class="alert alert-success">Data Updated Successfully.</div>');
+                    },
+                    error: function(xhr, status, error) {
+                        console.error(error);
+                        // Handle the error as needed
+                        // For example, display an error message
+                        $('.alert').remove();
+                        form.prepend('<div class="alert alert-danger">An error occurred while updating the data. Please try again later.</div>');
+                    }
+                });
+            });
+        });
+    </script>
 </head>
 <body>
     <h1 class="text-center mb-4">user_update</h1>
@@ -78,14 +105,14 @@ mysqli_close($conn);
         <div class="mb-3">
             <label for="name" class="form-label">Username</label>
             <input type="text" class="form-control" id="name" name="name" value="<?php echo $name; ?>">
-        </div> 
+        </div>
         <div class="mb-3">
-            <label for="roleid" class="form-label">Role ID:-</label>
+            <label for="roleid" class="form-label">Role ID:</label>
             <input type="text" class="form-control" id="roleid" name="roleid" value="<?php echo $roleid; ?>">
         </div>
-        <button type="submit" class="btn btn-success btn-block">Login</button>
+        <button type="submit" class="btn btn-success btn-block">Update</button>
     </form>
-    <?php if(isset($message)): ?>
+    <?php if (isset($message)): ?>
         <div class="alert mt-3 <?php echo $alertClass; ?>"><?php echo $message; ?></div>
     <?php endif; ?>
 </body>

1ac2134556ded9325f3420d18074e5be2bb4c274
Namra Nirajkumar Ravani <92981408+namraravani@users.noreply.github.com>
2023-05-29 00:16:32 -0700

    Update register.php

--- task-2/register.php
+++ task-2/register.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,82 +17,122 @@
         }
     </style>
 </head>
+
 <body>
-<div class="form-container">
-    <h1>Registration Form</h1>
-    <?php
-    $db = mysqli_connect("localhost", "root", "", "task-2");
+    <div class="form-container">
+        <h1>Registration Form</h1>
+        <?php
+        $db = mysqli_connect("localhost", "root", "", "task-2");
 
-    $roleid = "";
-    if (isset($_POST['name'])) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $mobileno = $_POST['mobileno'];
-        $address = $_POST['address'];
-        $roleid = $_POST['roleid'];
+        $roleid = "";
+        if (isset($_POST['submitted'])) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $mobileno = $_POST['mobileno'];
+            $address = $_POST['address'];
+            $roleid = $_POST['roleid'];
 
-        $sql = $db->query("INSERT INTO register (name, email, password, mobileno, address, roleid) VALUES ('$name', '$email', '$password', '$mobileno', '$address', '$roleid')");
+            $sql = $db->query("INSERT INTO register (name, email, password, mobileno, address, roleid) VALUES ('$name', '$email', '$password', '$mobileno', '$address', '$roleid')");
 
-        if ($sql) {
-            $message = "New person inserted successfully";
-            $alertClass = "alert-success";
-        } else {
-            $message = "Invalid details";
-            $alertClass = "alert-danger";
+            if ($sql) {
+                $message = "New person inserted successfully";
+                $alertClass = "alert-success";
+            } else {
+                $message = "Invalid details";
+                $alertClass = "alert-danger";
+            }
         }
-    }
 
-    // Fetch checkbox options from the database table
-    $roleQuery = $db->query("SELECT rolename FROM addrole");
-    $checkboxOptions = array();
-    while ($row = mysqli_fetch_assoc($roleQuery)) {
-        $checkboxOptions[] = $row['rolename'];
-    }
-    ?>
+        // Fetch checkbox options from the database table
+        $roleQuery = $db->query("SELECT rolename FROM addrole");
+        $checkboxOptions = array();
+        while ($row = mysqli_fetch_assoc($roleQuery)) {
+            $checkboxOptions[] = $row['rolename'];
+        }
+        ?>
 
-    <?php if (isset($message)) : ?>
-        <div class="alert <?php echo $alertClass; ?>"><?php echo $message; ?></div>
-    <?php endif; ?>
+        <?php if (isset($message)) : ?>
+            <div class="alert <?php echo $alertClass; ?>"><?php echo $message; ?></div>
+        <?php endif; ?>
 
-    <form action="" method="POST">
-        <div class="mb-3">
-            <label for="fullName" class="form-label">Full Name</label>
-            <input type="text" class="form-control" name="name">
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email address</label>
-            <input type="email" class="form-control" name="email" aria-describedby="emailHelp">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" name="password">
-        </div>
-        <div class="mb-3">
-            <label for="phone" class="form-label">Phone Number</label>
-            <input type="tel" class="form-control" name="mobileno">
-        </div>
-        <div class="mb-3">
-            <label for="address" class="form-label">Address</label>
-            <textarea class="form-control" name="address"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="roleSelect">Select Role :-</label>
-            <?php foreach ($checkboxOptions as $option) : ?>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="roleid" value="<?php echo $option; ?>">
-                    <label class="form-check-label"><?php echo $option; ?></label>
-                </div>
-            <?php endforeach; ?>
-        </div>
+        <form id="registrationForm" method="POST">
+            <input type="hidden" name="submitted" value="true"> <!-- Add a hidden input field to track form submission -->
+            <div class="mb-3">
+                <label for="fullName" class="form-label">Full Name</label>
+                <input type="text" class="form-control" name="name" id="name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" id="password">
+            </div>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone Number</label>
+                <input type="tel" class="form-control" name="mobileno" id="mobileno">
+            </div>
+            <div class="mb-3">
+                <label for="address" class="form-label">Address</label>
+                <textarea class="form-control" name="address" id="address"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="roleSelect">Select Role :-</label>
+                <?php foreach ($checkboxOptions as $option) : ?>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="roleid" value="<?php echo $option; ?>">
+                        <label class="form-check-label"><?php echo $option; ?></label>
+                    </div>
+                <?php endforeach; ?>
+            </div>
 
-        <button class="btn btn-outline-success" type="submit">SUBMIT</button>
-        <div class="sign-in">
-            <p>Already Have An Account ?<p><a href='login.php'>Signin</a>
-        </div>
-    </form>
-</div>
+            <button class="btn btn-outline-success" type="submit" name="submit">SUBMIT</button>
+            <div class="sign-in">
+                <p>Already Have An Account ?<p><a href='login.php'>Signin</a>
+            </div>
+        </form>
+    </div>
+
+    <script>
+    $(document).ready(function () {
+        $("#registrationForm").on("submit", function (e) {
+            e.preventDefault();
+
+            var formData = {
+                name: $("#name").val().trim(),
+                roleid: $("input[name='roleid']:checked").val(),
+                email: $("#email").val().trim(),
+                address: $("#address").val().trim(),
+                mobileno: $("#mobileno").val().trim(),
+                password: $("#password").val().trim(),
+                submitted: true
+            };
+
+            $.ajax({
+                url: "register.php",
+                type: "POST",
+                data: formData,
+                success: function (response) {
+                    console.log(response);
+                    var result = JSON.parse(response);
+                    var message = result.message;
+                    var alertClass = result.success ? 'alert-success' : 'alert-danger';
+                    $('.alert').remove();
+                    $('#registrationForm').prepend('<div class="alert ' + alertClass + '">' + message + '</div>');
+                },
+                error: function (xhr, status, error) {
+                    console.error(error);
+                    $('.alert').remove();
+                    $('#registrationForm').prepend('<div class="alert alert-danger">An error occurred. Please try again later.</div>');
+                }
+            });
+        });
+    });
+</script>
 
 
 </body>
+
 </html>
